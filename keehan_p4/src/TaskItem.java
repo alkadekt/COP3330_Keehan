b4cec8f89fe547bc9b83eaee3bbdb7c95406deee
Andrew Keehan <alkadekt@gmail.com>
2020-11-16 22:22:12 -0500

    Finishes the assignment

---
+++ keehan_p4/src/TaskItem.java
@@ -0,0 +1,90 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+import java.util.InputMismatchException;
+
+public class TaskItem {
+    private final String title;
+    private final String description;
+    private final String dueDate;
+    String status = "Uncompleted";
+
+    //Constructor
+    public TaskItem(String title, String description, String dueDate) {
+
+        if(isTitleValid(title)) {
+            this.title = title;
+        } else{
+            throw new InvalidTitleException("WARNING: title must be at least 1 character long; task not created");
+        }
+        if(isDescriptionValid(description)) {
+            this.description = description;
+        } else{
+            throw new InvalidDescriptionException("invalid description");
+        }
+        if(isDateValid(dueDate)) {
+            this.dueDate = dueDate;
+        } else {
+            throw new InvalidDateException("WARNING: invalid due date; task not created");
+        }
+    }
+
+    private boolean isTitleValid(String title) {
+        return title.length() > 0;
+    }
+
+    private boolean isDescriptionValid(String description) {
+        return description.length() >= 0;
+    }
+
+    private boolean isDateValid(String date) {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        format.setLenient(false);
+        try {
+            format.parse(date);
+        } catch (ParseException ex) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getDueDate() {
+        return this.dueDate;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void changeStatus(String newStatus) {
+        this.status = newStatus;
+    }
+}
+
+//EXCEPTION CLASSES
+class InvalidTitleException extends IllegalArgumentException {
+    public InvalidTitleException(String title) {
+        super(title);
+    }
+}
+
+class InvalidDescriptionException extends IllegalArgumentException {
+    public InvalidDescriptionException(String description) {
+        super(description);
+    }
+}
+
+class InvalidDateException extends IllegalArgumentException {
+    public InvalidDateException(String date) {
+        super(date);
+    }
+}
---
+++ keehan_p4/src/TaskList.java
@@ -0,0 +1,74 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    List<TaskItem> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+
+    public TaskItem get(int i) {
+        if(i<0) {
+            throw new IllegalArgumentException("index must be non negative");
+        }
+        if(i > tasks.size()) {
+            throw new IndexOutOfBoundsException("index is too big");
+        }
+        return tasks.get(i);
+    }
+
+    public void add(TaskItem item) {
+        tasks.add(item);
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public void printTaskList() {
+        System.out.println("Current Tasks");
+        System.out.println("-------------");
+        for(int i=0; i < size(); i++) {
+            if(tasks.get(i).getStatus() == "Complete") {
+                System.out.println(i + ") *** [" + tasks.get(i).getDueDate() + "] "
+                        + tasks.get(i).getTitle() + ": " + tasks.get(i).getDescription());
+            } else {
+                System.out.println(i + ") [" + tasks.get(i).getDueDate() + "] "
+                        + tasks.get(i).getTitle() + ": " + tasks.get(i).getDescription());
+
+            }
+        }
+    }
+
+    void printCompleteTaskList() {
+        System.out.println("Completed Tasks");
+        System.out.println("-------------");
+        for(int i=0; i < size(); i++) {
+            if(tasks.get(i).getStatus() == "Complete") {
+                System.out.println(i + ") [" + tasks.get(i).getDueDate() + "] "
+                        + tasks.get(i).getTitle() + ": " + tasks.get(i).getDescription());
+            }
+        }
+    }
+
+    void printUncompleteTaskList() {
+        System.out.println("Uncompleted Tasks");
+        System.out.println("-------------");
+        for(int i=0; i < size(); i++) {
+            if(tasks.get(i).getStatus() == "Uncomplete") {
+                System.out.println(i + ") [" + tasks.get(i).getDueDate() + "] "
+                        + tasks.get(i).getTitle() + ": " + tasks.get(i).getDescription());
+            }
+        }
+    }
+
+    public void changeItem(int index, TaskItem task) {
+        tasks.set(index, task);
+    }
+
+    public void trashItem(int index) {
+        tasks.remove(index);
+    }
+}
